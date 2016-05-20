b6b06b7b868bb60de80c45c3a910f8e9eb84a9b5
Takumi Neda <t@naninuneda.com>
2016-05-21 02:01:14 +0900

    First commit

---
+++ src/com/naninuneda/chofu/ChofuPower.java
@@ -0,0 +1,26 @@
+package com.naninuneda.chofu;
+
+
+import org.aiwolf.client.base.player.AbstractRoleAssignPlayer;
+
+import com.naninuneda.chofu.role.ChofuWerewolf;
+
+public class ChofuPower extends AbstractRoleAssignPlayer{
+
+	public ChofuPower(){
+
+		//setSeerPlayer(new ChofuSeer(this));
+		//setVillagerPlayer(new ChofuVillager(this));
+		//setBodyguardPlayer(new ChofuBodyguard(this));
+		setWerewolfPlayer(new ChofuWerewolf());
+		//setPossessedPlayer(new ChofuPossessed(this));
+
+	}
+
+
+	@Override
+	public String getName() {
+		return "ChofuPower";
+	}
+
+}
