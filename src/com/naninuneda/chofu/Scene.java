d8e03f045a1b086c205a802e693df8f557c28459
Takumi Neda <t@naninuneda.com>
2016-05-22 01:15:25 +0900

    make Scene, Scenes Class

---
+++ src/com/naninuneda/chofu/Scene.java
@@ -0,0 +1,17 @@
+package com.naninuneda.chofu;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.aiwolf.common.data.Agent;
+import org.aiwolf.common.data.Role;
+
+public class Scene {
+
+	private HashMap<Agent,Role> map;
+
+	public Scene(){
+		map = new HashMap<Agent,Role>();
+	}
+
+}
